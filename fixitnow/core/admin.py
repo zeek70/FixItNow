95eca40532d10c3770435bfd5e614f6ee1af6762
zeek70 <zaxbaaba@gmail.com>
2025-08-26 23:52:28 +0100

    Clean up fixitnow submodule reference

--- fixitnow/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ServiceUpdate,User,ServiceRequest,Category
-
-@admin.register(ServiceUpdate)
-class ServiceUpdateAdmin(admin.ModelAdmin):
-    list_display=('id','servicerequest','description','created_at')
-
-# Register your models here.
